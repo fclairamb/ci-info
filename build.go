4cd99dc40891e0c336a872215dd87bed7861997e
Florent Clairambault <florent@clairambault.fr>
2022-04-24 00:31:03 +0200

    Something's not right 🤔

--- build.go
+++ build.go
@@ -1,12 +1,12 @@
 package main
 
 const (
-    // BUILD_VERSION is the current version of the program
-    BUILD_VERSION = "0.1.0-main-544bf25"
+	// BUILD_VERSION is the current version of the program
+	BUILD_VERSION = "0.0.0"
 
-    // BUILD_TIME is the time the program was built
-    BUILD_TIME = "2022-04-23-2125"
+	// BUILD_TIME is the time the program was built
+	BUILD_TIME = "0000-00-00-0000"
 
-    // COMMIT_SMART is the git hash of the program
-    COMMIT_SMART = "main-544bf25"
+	// COMMIT_SMART is the git hash of the program
+	COMMIT_SMART = "main-0000000"
 )
