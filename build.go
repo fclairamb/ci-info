b298ebcfc097464b097b270bb1ffca1c5b963e61
Florent Clairambault <florent@clairambault.fr>
2022-04-24 14:24:28 +0200

    restoring build.go

---
+++ build.go
@@ -0,0 +1,12 @@
+package main
+
+const (
+	// BuildVersion is the current version of the program
+	BuildVersion = "0.0.0"
+
+	// BuildDate is the time the program was built
+	BuildDate = "0000-00-00-0000"
+
+	// Commit is the git hash of the program
+	Commit = "0000000000000000000000000000000000000000"
+)
