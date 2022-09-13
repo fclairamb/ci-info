e6512ddebab08801fe5ac48eab66dde35820d4cd
Florent Clairambault <florent@clairambault.fr>
2022-09-13 17:51:44 +0200

    chore(CI): Removing temporary build info

--- build.go
+++ build.go
@@ -2,11 +2,11 @@ package main
 
 const (
 	// BuildVersion is the current version of the program
-	BuildVersion = "0.1.0-feature-various-improvements-3021755"
+	BuildVersion = ""
 
 	// BuildDate is the time the program was built
-	BuildDate = "2022-09-13-1326"
+	BuildDate = ""
 
 	// Commit is the git hash of the program
-	Commit = "feature-various-improvements-3021755"
+	Commit = ""
 )
