ba796493bc0b9aed729559aa6a5670a1c6cfc34f
Florent Clairambault <florent@clairambault.fr>
2022-09-13 15:27:05 +0200

    Reverting build info changes

--- build.go
+++ build.go
@@ -2,11 +2,11 @@ package main
 
 const (
 	// BuildVersion is the current version of the program
-	BuildVersion = "0.1.0-feature-config-change-6ea1772"
+	BuildVersion = "0.1.0-feature-various-improvements-3021755"
 
 	// BuildDate is the time the program was built
-	BuildDate = "2022-09-11-0007"
+	BuildDate = "2022-09-13-1326"
 
 	// Commit is the git hash of the program
-	Commit = "feature-config-change-6ea1772"
+	Commit = "feature-various-improvements-3021755"
 )
