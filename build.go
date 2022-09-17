69ef63eb6d6f4b450805dda0f7e223df4f8e6fa4
Florent Clairambault <florent@clairambault.fr>
2022-09-18 01:27:36 +0200

    fix(log): Allowing to set the logging level (#30)
    * feature(log): Allowing to set the logging level

--- build.go
+++ build.go
@@ -2,11 +2,11 @@ package main
 
 var (
 	// BuildVersion is the current version of the program
-	BuildVersion = "0.4.3"
+	BuildVersion = "0.4.3-feature-log-b8d7684"
 
 	// BuildDate is the time the program was built
-	BuildDate = "2022-09-17T22:30:34Z"
+	BuildDate = "2022-09-17T23:24:30Z"
 
 	// Commit is the git hash of the program
-	Commit = "v0.4.3"
+	Commit = "feature-log-b8d7684"
 )
---
+++ log.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	log15 "github.com/inconshreveable/log15"
+)
+
+// Log is the logger
+var log log15.Logger
+
+func init() { //nolint:gochecknoinits
+	log = log15.New()
+}
+
+// SetLoggingLevel sets the log level
+func SetLoggingLevel(level string) error {
+	lvl, err := log15.LvlFromString(level)
+	if err != nil {
+		return err
+	}
+
+	log.SetHandler(log15.LvlFilterHandler(lvl, log15.StdoutHandler))
+
+	return nil
+}
