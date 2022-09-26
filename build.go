d90846b294cba4194cc5c901ca8f500e31b45d7e
Florent Clairambault <florent@clairambault.fr>
2022-09-26 15:18:55 +0200

    test fix

--- build.go
+++ build.go
@@ -2,11 +2,11 @@ package main
 
 var (
 	// BuildVersion is the current version of the program
-	BuildVersion = "0.5.0-chore-changing_git_date-70f93de"
+	BuildVersion = "0.5.0-chore-changing_git_date-089da9c"
 
 	// BuildDate is the time the program was built
-	BuildDate = "2022-09-26T13:17:07+02:00"
+	BuildDate = "2022-09-26T15:18:21+02:00"
 
 	// Commit is the git hash of the program
-	Commit = "chore-changing_git_date-70f93de"
+	Commit = "chore-changing_git_date-089da9c"
 )
